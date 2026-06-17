668068361773d1f6647190175f332c338fab141c
Aiden Bohlander <aiden.bohlander@gmail.com>
2026-06-16 17:26:59 -0700

    little updates

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import os
 import shutil
+import time
+import uuid
 from pathlib import Path
 
 import dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+from slack_sdk.errors import SlackApiError
 
 from cobalt_api import CobaltAPIClient, CobaltAPIError, CobaltAPIClientException
 
@@ -12,8 +15,7 @@ dotenv.load_dotenv()
 app = App(token=os.environ["SLACK_BOT_TOKEN"])
 client = app.client
 cobalt_client = CobaltAPIClient(os.environ["COBALT_API_INSTANCE"], os.environ.get("COBALT_API_KEY"))
-OUTPUT_DIR = os.environ.get("OUTPUT_DIR")
-OUTPUT_DIR = "output" if not OUTPUT_DIR else OUTPUT_DIR
+OUTPUT_DIR = os.environ.get("OUTPUT_DIR", "output")
 
 
 def recursive_url_search(elements):
@@ -34,15 +36,29 @@ def raw_file_reply(message):
     if not blocks:
         return
     urls = recursive_url_search(blocks)
+    print(urls)
     ts = message["ts"]
     for url in urls:
         save_path = None
+        if not cobalt_client.supports_url(url):
+            continue
         try:
             cobalt_response = cobalt_client.post(url)
-            save_path = str(Path(OUTPUT_DIR) / Path(cobalt_response.filename))
+            save_path = str(Path(OUTPUT_DIR) / Path(str(uuid.uuid4())))
             cobalt_response.stream_to_file(save_path)
-            print(message["channel"], save_path, ts)
-            client.files_upload_v2(channel=message["channel"], file=save_path, thread_ts=ts)
+            permalink = client.files_upload_v2(file=save_path, filename=cobalt_response.filename)["file"]["permalink"]
+            blocks = [
+                {
+                    "type": "context",
+                    "elements": [
+                        {
+                            "type": "mrkdwn",
+                            "text": f"Shared by <@{message['user']}> | <{permalink}|Link to file> | <https://TheTridentGuy/slack-cobalt|:star: on GitHub>"
+                        }
+                    ]
+                }
+            ]
+            client.chat_postMessage(channel=message["channel"], thread_ts=ts, blocks=blocks)
             os.remove(save_path)
         except (CobaltAPIError, CobaltAPIClientException) as e:
             if save_path:
--- cobalt_api/cobalt_api.py
+++ cobalt_api/cobalt_api.py
@@ -1,25 +1,55 @@
 from typing import BinaryIO, Any
 
 import requests
+import re
+
+
+_COBALT_HOST_REGEX = re.compile(
+    r"(?:m\.)?bilibili\.com"
+    r"|(?:profile\.)?bsky\.app"
+    r"|dailymotion\.com"
+    r"|(?:web|m\.)?facebook\.com"
+    r"|fb\.watch"
+    r"|(?:ddinstagram\.com|instagram\.com)"
+    r"|loom\.com"
+    r"|ok\.ru"
+    r"|pinterest\.com"
+    r"|pin\.it"
+    r"|newgrounds\.com"
+    r"|reddit\.com"
+    r"|(?:rutube\.ru|rutube\.com)"
+    r"|snapchat\.com"
+    r"|(?:on|m\.)?soundcloud\.com"
+    r"|streamable\.com"
+    r"|(?:vt|vm|m|t|pro\.)?tiktok\.com"
+    r"|(?:tumblr\.com|www\.tumblr\.com)"
+    r"|(?:clips|www|m\.)?twitch\.tv"
+    r"|(?:x\.com|twitter\.com|vxtwitter\.com|fixvx\.com|fixupx\.com)"
+    r"|(?:player\.)?vimeo\.com"
+    r"|(?:vk\.com|vkvideo\.ru|vk\.ru)"
+    r"|youtu\.be"
+    r"|(?:music|m\.)?youtube\.com",
+    re.IGNORECASE
+)
 
 
 class CobaltAPIStreamableResponse:
-    def __init__(self, stream: requests.Response, original_response_json: Any):
+    def __init__(self, stream: requests.Response, original_response_json: Any) -> None:
         self.stream = stream
         self._original_response_json = original_response_json
 
     @property
-    def filename(self):
+    def filename(self) -> str:
         return self._original_response_json["filename"]
 
-    def stream_to_file(self, file: BinaryIO | str, chunk_size=8192):
+    def stream_to_file(self, file: BinaryIO | str, chunk_size=8192) -> None:
         if isinstance(file, str):
             file = open(file, "wb")
         with file as f:
             for chunk in self.stream.iter_content(chunk_size=chunk_size):
                 f.write(chunk)
             if f.tell() == 0:
-                raise CobaltAPIError("Response stream was empty.")
+                raise CobaltAPIClientException("Response stream was empty.")
 
 
 class CobaltAPITunnelResponse(CobaltAPIStreamableResponse):
@@ -35,7 +65,7 @@ class CobaltAPIClient:
         self.instance_url = instance_url
         self.api_key = api_key
 
-    def post(self, url: str) -> CobaltAPITunnelResponse | CobaltAPIRedirectResponse | None:
+    def post(self, url: str) -> CobaltAPITunnelResponse | CobaltAPIRedirectResponse:
         headers = {
             "Accept": "application/json",
             "Content-Type": "application/json"}
@@ -63,6 +93,10 @@ class CobaltAPIClient:
             return CobaltAPIRedirectResponse(stream, response)
         else:
             raise CobaltAPIClientException(f"Unsupported non-error status: {status}")
+
+    @staticmethod
+    def supports_url(url: str) -> bool:
+        return False if _COBALT_HOST_REGEX.search(url) is None else True
 
 
 class CobaltAPIError(Exception):
